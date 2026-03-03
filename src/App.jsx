20242e570989462be8e72536db3da2759dde9ed4
ThomasNg <junyongiiing037@gmail.com>
2026-03-03 13:27:51 +0800

    feat: Cart Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import CartPage from './pages/CartPage' 
 
 function App() {
-  return <HomePage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
