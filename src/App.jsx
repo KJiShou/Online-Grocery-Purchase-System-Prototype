dbc66896baaa635d6937ac8e2aba325f509d7701
noir <130840857+yomi2998@users.noreply.github.com>
2026-03-05 17:16:58 +0800

    add GroceryListPage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PaymentPage from './pages/PaymentPage'
 import SelectPaymentPage from './pages/SelectPaymentPage'
 import SelectVoucherPage from './pages/SelectVoucherPage'
 import CategoryPage from './pages/CategoryPage'
+import GroceryListPage from './pages/GroceryListPage'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/categories" element={<CategoryPage title="Categories Page" />} />
+        <Route path="/grocery-list" element={<GroceryListPage title="Grocery List Page" />} />
         <Route path="/cart" element={<CartPage title="" />} />
         <Route path="/wishlist" element={<PlaceholderPage title="Wishlist Page" />} />
         <Route path="/profile" element={<PlaceholderPage title="Profile Page" />} />
